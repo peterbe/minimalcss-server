66f363b0c5d3e2b7d11071aa9b1d036ddecd7b75
Peter Bengtsson <mail@peterbe.com>
2018-02-28 15:19:07 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# minimalcss-server
+
+A Node Express server with a pool of `puppeteer` browsers
+sent into [`minimalcss`](https://github.com/peterbe/minimalcss) that
+analyzes the minimal CSS for a URL.
+
+To run:
+
+```sh
+$ node server.js
+```
+
+To test:
+
+```sh
+curl -X POST  -H 'Content-Type: application/json' \
+  -d '{"url": "https://news.ycombinator.com"}' \
+  http://localhost:5000/minimize
+```
+
+It uses a pool of opened puppeteer `browser` instances created with
+launch args `args: ['--no-sandbox', '--disable-setuid-sandbox']`.
+These are then sent to `minimalcss` and its output is returned as JSON
+by this server. It looks something like this:
+
+```json
+{
+    "result": {
+        "finalCss": "[SNIP]",
+        "stylesheetContents": {
+            "https://news.ycombinator.com/news.css?tcSCuEkwhnkVn0z07QWM":
+                "[SNIP]"
+        },
+        "_url": "https://news.ycombinator.com",
+        "_took": 3454.6355110000004,
+        "_cache": "miss"
+    }
+}
+```
+
+An LRU cache protects for repeated calls. If there's a cache hit on the LRU
+the exact same output is returned by `result._took === 0.0` and
+`result._cache === 'hit'`.
+
+## License
+
+Copyright (c) 2017-2018 [Peter Bengtsson](https://www.peterbe.com).
+See the [LICENSE](/LICENSE) file for license rights and limitations (MIT).
---
+++ server.js
@@ -0,0 +1,125 @@
+const express = require('express');
+const path = require('path');
+const responseTime = require('response-time');
+const genericPool = require('generic-pool');
+const now = require('performance-now');
+const puppeteer = require('puppeteer');
+const minimalcss = require('minimalcss');
+const LRU = require('lru-cache');
+const morgan = require('morgan');
+
+const PORT = process.env.PORT || 5000;
+
+const LRUCache = LRU({
+  max: 10,
+  // length: function (n, key) { return n * 2 + key.length }
+  // , dispose: function (key, n) { n.close() }
+  maxAge: 1000 * 60 * 60
+});
+
+const factory = {
+  create: async function() {
+    // const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    return browser;
+  },
+  destroy: function(puppeteer) {
+    puppeteer.close();
+  }
+};
+
+const browserPool = genericPool.createPool(factory, {
+  max: 10,
+  min: 2,
+  maxWaitingClients: 50
+});
+
+const app = express();
+
+// This sets an 'X-Response-Time' header to every request.
+app.use(responseTime());
+
+// So we can parse JSON bodies
+// app.use(bodyParser());
+app.use(express.json());
+
+app.post('/minimize', async function(req, res) {
+  const url = req.body.url;
+  res.set('Content-Type', 'application/json');
+  const cached = LRUCache.get(url);
+  if (cached) {
+    const full = JSON.parse(cached);
+    full.result._took = 0.0;
+    full.result._cache = 'hit';
+    res.send(JSON.stringify(full));
+    return;
+  }
+
+  if (!url) {
+    res.status(400);
+    res.send(
+      JSON.stringify({
+        error: "missing 'url' in JSON body"
+      })
+    );
+  } else {
+    const browser = await browserPool.acquire();
+    const t0 = now();
+    try {
+      await minimalcss
+        .minimize({
+          urls: [url],
+          browser: browser
+        })
+        .then(result => {
+          // browser.close();
+          browserPool.release(browser);
+          const t1 = now();
+          result._url = url;
+          result._took = t1 - t0;
+
+          LRUCache.set(url, JSON.stringify({ result }));
+          result._cache = 'miss';
+          res.send(
+            JSON.stringify({
+              result
+            })
+          );
+        })
+        .catch(error => {
+          // browser.close();
+          // await browserPool.release(browser);
+          browserPool.release(browser);
+          console.error(`Failed the minimize CSS: ${error}`);
+          res.status(500);
+          res.send(
+            JSON.stringify({
+              error: error.toString()
+            })
+          );
+        });
+    } catch (ex) {
+      browserPool.release(browser);
+      console.error(ex);
+      res.status(500);
+      res.send(
+        JSON.stringify({
+          error: error.toString()
+        })
+      );
+    }
+  }
+});
+
+app.get('/', async function(req, res) {
+  res.set('Content-Type', 'text/plain');
+  res.send('Yeah, it works.\n');
+});
+
+app.listen(PORT, function() {
+  console.error(
+    `Node cluster worker ${process.pid}: listening on port ${PORT}`
+  );
+});
